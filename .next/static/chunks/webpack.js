9cc18e9ebc0090ba868f2e29ff7c383cd74ed5f2
KhalidHosein <khalid+github@t2marcom.com>
2023-10-01 11:29:33 -0400

    contact update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "36bc87c5e50e676a"; }
+/******/ 		__webpack_require__.h = function() { return "5c5ba3b16da0609f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Contact.jsx
+++ components/Contact.jsx
@@ -13,35 +13,54 @@ const Contact = () => {
   }, []);
   return (
     <div className="container px-4 mx-auto pt-16 pb-8">
-     <AnimatedElement animationType="fadeInUp">
-      <p className={`uppercase text-right ${josefin.className}`}>Get in Touch</p>
-      <h2 className={`mb-5 font-bold text-right ${prompt.className}`}>Let's Connect.</h2>
-     </AnimatedElement>
+      <AnimatedElement animationType="fadeInUp">
+        <p className={`uppercase text-right ${josefin.className}`}>
+          Get in Touch
+        </p>
+        <h2 className={`mb-5 font-bold text-right ${prompt.className}`}>
+          Let's Connect.
+        </h2>
+      </AnimatedElement>
       <div className="flex flex-col-reverse gap-20 lg:flex-row items-center">
-      
         <div className="w-full lg:w-1/2">
-        <AnimatedElement animationType="fadeInLeft">
-          <InlineWidget
-            url="https://calendly.com/khalid-hosein/get-to-know-me"
-            styles={{
-              height: "1200px",
-              minHeight: "1200px", // Adjust the height to prevent content overflow
-            }}
-          />
-        </AnimatedElement>
+          <AnimatedElement animationType="fadeInLeft">
+            <InlineWidget
+              url="https://calendly.com/khalid-hosein/get-to-know-me"
+              styles={{
+                height: "1200px",
+                minHeight: "1200px", // Adjust the height to prevent content overflow
+              }}
+            />
+          </AnimatedElement>
         </div>
         <div className="w-full lg:w-1/2">
-        <AnimatedElement animationType="fadeInRight">
-          <p className={`text-xl leading-8 mt-5 ${josefin.className}`}>
-            I'm excited to hear from you. Whether you have a project in mind, a
-            question about web development, or just want to chat about the
-            latest tech trends, I'm all ears! Feel free to reach out and let's
-            start a conversation. The digital world is full of possibilities,
-            and I can't wait to explore them together. Don't hesitate to drop me
-            a message using the contact form or through the provided social
-            links. Looking forward to connecting with you!
-          </p>
-        </AnimatedElement>
+          <AnimatedElement animationType="fadeInRight">
+          <div className="flex">
+            <div className="avatar online h-20 mt-2">
+              <div className="hover:ring-4 relative">
+                <img
+                  className="rounded-full"
+                  src="/images/khavatar.png"
+                  alt="Khalid Hosein"
+                />
+              </div>
+            </div>
+            <div className="chat chat-start">
+              <div className="chat-bubble bg-ship-cove-500">
+                <p className={`text-xl leading-8 p-10 ${josefin.className}`}>
+                  I'm excited to hear from you. Whether you have a project in
+                  mind, a question about web development, or just want to chat
+                  about the latest tech trends, I'm all ears! Feel free to reach
+                  out and let's start a conversation. The digital world is full
+                  of possibilities, and I can't wait to explore them together.
+                  Don't hesitate to drop me a message using the contact form or
+                  through the provided social links. Looking forward to
+                  connecting with you!
+                </p>
+              </div>
+            </div>
+          </div>
+          </AnimatedElement>
         </div>
       </div>
     </div>

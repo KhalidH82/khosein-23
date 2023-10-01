a314156dc15fd839587a3512a40b180bdf4b703e
KhalidHosein <khalid+github@t2marcom.com>
2023-10-01 18:16:38 -0400

    footer fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "421047fbc0a45573"; }
+/******/ 		__webpack_require__.h = function() { return "b397fa7525c6ca91"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { footerLinks } from '../app/constants/data'
 const Footer = () => {
   return (
-    <footer className="w-full h-[64px] bg-ship-cove-500 text-ship-cove-300">
+    <footer className="w-full h-[64px] text-ship-cove-300">
       <div className="h-full w-full flex justify-center items-center gap-5">
         {footerLinks.map((link) => (
           <a href={link.link}>

0b7b245e96b63a7fac684953a9c8cdbcb647639e
KhalidHosein <khalid+github@t2marcom.com>
2023-10-08 11:09:28 -0400

    client update

--- components/Client.jsx
+++ components/Client.jsx
@@ -5,13 +5,13 @@ import { prompt, josefin } from "../app/fonts";
 
 const Clients = ({ client }) => {
   return (
-    <div className="py-24">
+    <div className="h-screen py-24">
       {client.map((c) => (
         <>
           <p className={`uppercase text-right mb-5 ${josefin.className}`}>
             Client
           </p>
-          <div className="card bg-ship-cove-200 px-10 py-8 drop-shadow-2xl">
+          <div className="card bg-ship-cove-200 p-5 drop-shadow-2xl">
             <a
               href={c.link}
               className="flex items-center justify-end underline lg:no-underline"
@@ -22,19 +22,21 @@ const Clients = ({ client }) => {
                   className="w-24 h-24 object-contain px-5"
                 />
               </button>
-              <h2 className={`font-bold leading-[1.3] ${prompt.className}`}>
-                {c.name}
-              </h2>
+              <div className="flex flex-col relative top-3">
+                <h2 className={`font-bold leading-[1.3] ${prompt.className}`}>
+                  {c.name}
+                </h2>
+                <p className={`uppercase text-right ${josefin.className}`}>
+                  {c.date}
+                </p>
+              </div>
             </a>
-            <p className={`uppercase text-right ${josefin.className}`}>
-              {c.date}
-            </p>
-            <div className="mt-12">
+            <div className="mt-5">
               <p className={`text-xl leading-1 ${josefin.className}`}>
                 {c.description}
               </p>
             </div>
-            <div className="mt-12">
+            <div className="mt-5">
               {c.testimonials.map((testimonial) => (
                 <div className="flex">
                   <div className="avatar online h-20 mt-2">
